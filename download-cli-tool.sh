8e76a2c7dc340ce9862ce2998d82417bfd91f3c6
Nándor Holozsnyák <nandor.holozsnyak@gmail.com>
2023-04-24 20:51:57 +0200

    feat(core): GitHub action for release
    - GitHub action should inherit the version of the CLI from itself

--- download-cli-tool.sh
+++ download-cli-tool.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VERSION=$1
-TAG="v${VERSION}"
+TAG="${VERSION}"
 if [ $VERSION == 'early-access' ]; then
     TAG="early-access"
     VERSION="999-SNAPSHOT"
+else
+    VERSION=${VERSION#?}
 fi
 
 wget https://github.com/rodnansol/jbang-catalog-document-generator/releases/download/${TAG}/jbang-catalog-document-generator-cli-${VERSION}.jar -O jbang-catalog-document-generator-cli.jar
